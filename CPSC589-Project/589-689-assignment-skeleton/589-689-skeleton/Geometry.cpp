4c91fa6a640320e4f2e97f37061cb1c2b25aa999
Uijin <uijin.park1@ucalgary.ca>
2024-03-09 16:07:34 -0400

    initialize

---
+++ CPSC589-Project/589-689-assignment-skeleton/589-689-skeleton/Geometry.cpp
@@ -0,0 +1,49 @@
+#include "Geometry.h"
+
+#include <utility>
+
+
+GPU_Geometry::GPU_Geometry()
+	: vao()
+	, vertBuffer(0, 3, GL_FLOAT)
+	, colBuffer(1, 3, GL_FLOAT)
+{}
+
+
+void GPU_Geometry::setVerts(const std::vector<glm::vec3>& verts) {
+	vertBuffer.uploadData(sizeof(glm::vec3) * verts.size(), verts.data(), GL_STATIC_DRAW);
+}
+
+
+void GPU_Geometry::setCols(const std::vector<glm::vec3>& cols) {
+	colBuffer.uploadData(sizeof(glm::vec3) * cols.size(), cols.data(), GL_STATIC_DRAW);
+}
+
+/*
+#include "Geometry.h"
+
+#include <utility>
+
+
+GPU_Geometry::GPU_Geometry()
+	: vao()
+	, vertBuffer(0, 3, GL_FLOAT)
+	, colBuffer(1, 3, GL_FLOAT)
+{}
+
+
+void GPU_Geometry::setVerts(const std::vector<std::vector<glm::vec3>>& verts) {
+	for (const auto& v : verts) {
+		size_t vSize = v.size() * sizeof(glm::vec3);
+		vertBuffer.uploadData(vSize, v.data(), GL_DYNAMIC_DRAW);
+	}
+}
+
+
+void GPU_Geometry::setCols(const std::vector<std::vector<glm::vec3>>& cols) {
+	for (const auto& c : cols) {
+		size_t cSize = c.size() * sizeof(glm::vec3);
+		colBuffer.uploadData(cSize, c.data(), GL_DYNAMIC_DRAW);
+}
+
+*/
